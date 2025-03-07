154ae6d0ef099c3b0caaeee92679646cf8c7c194
thaliadani <thaliadani2@gmail.com>
2025-03-07 16:55:26 -0300

    deleted:    dist/image_processing-0.0.1-py3-none-any.whl 	deleted:    dist/image_processing-0.0.1.tar.gz 	deleted:    image_processing.egg-info/PKG-INFO 	deleted:    image_processing.egg-info/SOURCES.txt 	deleted:    image_processing.egg-info/dependency_links.txt 	deleted:    image_processing.egg-info/requires.txt 	deleted:    image_processing.egg-info/top_level.txt 	deleted:    image_processing/__init__.py 	deleted:    image_processing/processing/__init__.py 	deleted:    image_processing/processing/combination.py 	deleted:    image_processing/processing/transformation.py 	deleted:    image_processing/utils/__init__.py 	deleted:    image_processing/utils/io.py 	deleted:    image_processing/utils/plot.py 	renamed:    build/lib/image_processing/__init__.py -> image_processing_Thalia/__init__.py 	renamed:    build/lib/image_processing/processing/__init__.py -> image_processing_Thalia/processing/__init__.py 	renamed:    build/lib/image_processing/processing/combination.py -> image_processing_Thalia/processing/combination.py 	renamed:    build/lib/image_processing/processing/transformation.py -> image_processing_Thalia/processing/transformation.py 	renamed:    build/lib/image_processing/utils/__init__.py -> image_processing_Thalia/utils/__init__.py 	renamed:    build/lib/image_processing/utils/io.py -> image_processing_Thalia/utils/io.py 	renamed:    build/lib/image_processing/utils/plot.py -> image_processing_Thalia/utils/plot.py 	modified:   setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="image_processing",
+    name="image_processing_Thalia",
     version="0.0.1",
     author="Thalia",
     author_email="thaliadani2@gmail.com",
